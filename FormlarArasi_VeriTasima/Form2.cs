a1797e351724a0f9d29a25bcab297e316debcae8
m.sinans <msomercikoglu@gmail.com>
2023-03-17 11:29:58 +0300

    Add project files.

---
+++ FormlarArasi_VeriTasima/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormlarArasi_VeriTasima
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public string mesaj;
+        public string gond;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = gond;
+            label2.Text = mesaj;
+        }
+    }
+}
